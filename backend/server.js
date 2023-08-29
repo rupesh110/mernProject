e340d1ee80486b3a5fbf167edad2918d3e091f2c
Rupesh <shrestharupesh110@gmail.com>
2023-08-30 01:08:33 +1000

    custom error handler

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
 
+import userRoutes from './routes/userRoutes.js';
+import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res) => { res.send("API is running...")})
 
