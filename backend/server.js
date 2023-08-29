738d20190ec0939e262363bf74d356fe36c2f7f6
Rupesh <shrestharupesh110@gmail.com>
2023-08-30 01:15:30 +1000

    database connected

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 
 import userRoutes from './routes/userRoutes.js';
 import {notFound, errorHandler} from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
 
 const port = process.env.PORT || 5000;
+connectDB();
 const app = express();
 app.use('/api/users', userRoutes);
 app.use(notFound);
